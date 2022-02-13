5d6e20064d07f39f9abc9c4e2d3b290dc1e68178
Shalbedo <sozidatel909@mail.ru>
2022-02-13 20:18:36 +0700

    Connecting to the database

--- Form.php
+++ Form.php
@@ -1,8 +1,9 @@
 <form action="index.php" method="post">
-Name: <input type="text" name="name">
-E-mail: <input type="text" name="email">
+Name: <input type="text" name="name"><br><br>
+E-mail: <input type="text" name="email"><br><br>
 Gender: <input type="radio" name="gender" value="male">Male
 <input type="radio" name="gender" value="female">Female
-<input type="radio" name="gender" value="helicopter">Attack Helicopter AH-64A "Apache"
-Comment: <textarea name="comment" rows="2" cols="30"></textarea>
+<input type="radio" name="gender" value="helicopter">Attack Helicopter AH-64A "Apache"<br><br>
+Comment: <textarea name="comment" rows="2" cols="30"></textarea><br><br>
+<button type="submit">Подтверждаю, что данные верны и я готов зайти в /b</button>
 </form>
--- index.php
+++ index.php
@@ -1,4 +1,39 @@
 <?php
+include 'Form.php';
+$dsn= "mysql:dbname=main;host=database";
+$name=$_POST['name'];
+$email=$_POST['email'];
+$gender=$_POST['gender'];
+$comment=$_POST['comment'];
+$username= "root";
+$password= "password";
+try {
+    $db= new PDO($dsn, $username, $password); // Переменная с текущим подключением к базе данных
+    echo "suca\n";
+} catch (PDOException $e) {
+    echo $e->getMessage();
+}
+function createTable ($db) {
+    $statement=$db->prepare("CREATE TABLE IF NOT EXISTS user (
+        Name varchar(255),
+        Email varchar(255),
+        Gender varchar(255),
+        Comment varchar(255),
+        )");
+    $statement->execute();
+}
+function addRecord ($db, $name, $email, $gender, $comment) {
+    $statement=$db->prepare("insert into user(Name,Email,Gender,Comment) values (?,?,?,?)");
+    $statement->execute([$name, $email, $gender, $comment]);
+}
+function showTable ($db){
+    $statement=$db->prepare("select * from user");
+    $statement->execute(); // execute - выполняет запрос к БД/
+    while ($data=$statement->fetch()) { // fetch - получить результат выполнения запроса (одну строчку)
+        print_r($data);
+    }
+}
+createTable($db);
 
 if ($_SERVER["REQUEST_METHOD"]==="POST") {
     if (!isset($_POST["name"])){
@@ -24,3 +59,6 @@ function checktext($data)
     $data=htmlspecialchars($data); // Преобразует специальные символы в HTML сущности
     return $data;
 }
+
+addRecord ($db, $name, $email, $gender, $comment);
+showTable ($db);
